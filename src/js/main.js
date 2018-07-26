f5b1dc915574a6285fa7a424701b6f4c1c9b49f4
Artem Klochkov <artem.klochkov@soft-werke.com>
2018-07-26 19:38:59 +0300

    Add crap

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 
+/*
+const localStorageContent = localStorage.getItem('users');
+
+let users;
+
+if(localStorageContent === null) {
+    users = [];
+} else {
+    users = JSON.parse( localStorageContent );
+}
+
+var obj = {
+    name: 'Nick',
+    age: 44
+}
+
+users.push(obj);
+
+
+localStorage.setItem('users', JSON.stringify( users ) );
+*/
+
+/*localStorage.setItem('users', person);*/
+
