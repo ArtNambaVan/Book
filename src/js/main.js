2ce062b604c235df30084f0c90c932584590ecc0
Artem Klochkov <artem.klochkov@soft-werke.com>
2018-07-31 14:17:02 +0300

    Add mediator

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,34 @@
-$(document).ready(function(){
+
+    var mediator = (function() {
+
+        var subscribers = {};
+
+        var subscribe = function(eventName, fn) {
+            this.subscribers[eventName] = this.subscribers[eventName] || [];
+            this.subscribers[eventName].push(fn);
+        }
+
+        var unsubscribe = function(eventName,fn) {
+            // code
+        }
+
+        var publish = function(eventName, data) {
+            if (this.subscribers[eventName]) {
+                this.subscribers[eventName].forEach(function(fn) {
+                    fn(data)
+                })
+            }
+        }
+
+        return {
+            subscribers,
+            subscribe: subscribe,
+/*            unsubscribe: unsubscribe,*/
+            publish: publish
+        }
+
+    }());
+
 
 
     var usersDate = (function() {
@@ -10,7 +40,6 @@ $(document).ready(function(){
             email    : 'Arthorror@gmail.com',
             password : 'Anthrax1'
         }
-
         obj1 = {
             name     : 'Vadim',
             email    : 'Vadim@gmail.com',
@@ -29,10 +58,41 @@ $(document).ready(function(){
 
 
 
+
+    var booksDate = (function() {
+
+
+        var getBookLocalStorage = function() {
+            var books,
+            booksLS = localStorage.getItem('books');
+
+            if( booksLS === null ) {
+                books = [];
+            } else {
+                books = JSON.parse( booksLS )
+            }
+
+            return books;
+        }
+
+        var addBookLocalStorage = function(book) {
+            var books = getBookLocalStorage();
+            books.push(book)
+
+            localStorage.setItem( 'tweets', JSON.stringify(books) );
+        }
+
+        return {
+            getBookLocalStorage: getBookLocalStorage(),
+            addBookLocalStorage : addBookLocalStorage()
+        }
+
+    })()
+
+
     var usersController = (function() {
 
         var DOMstrings = {
-            inputName     : '#inputName',
             inputEmail    : '#inputEmail',
             inputPassword : '#inputPassword',
             loginForm     : '#login-form'
@@ -43,6 +103,7 @@ $(document).ready(function(){
             loginForm.addEventListener('submit', function(e) {
                 e.preventDefault();
                 checkUser();
+
             })
 
 
@@ -57,65 +118,68 @@ $(document).ready(function(){
                 if (email === allUsers[i].email && password === allUsers[i].password) {
                     $('#modalLoginForm').modal("hide");
                     alert('hello ' + allUsers[i].name);
-                    return
+                    mediator.publish('userLogin', 'data')
                 }
             }
-
         }
-
-        return {
-            getDOMstrings: function() {
-                return DOMstrings;
-            }
-        }
-
     })();
+
 
     var bookController = (function(){
 
-      var title           = document.querySelector('#title');
-      var description     = document.querySelector('#description');
-      var genre           = document.querySelector('#genre');
-      var type            = document.querySelectorAll('input[name="type"]');
-      var bookForm        = document.querySelector('#book-form');
-      var typeValue;
+        var title           = document.querySelector('#title'),
+            description     = document.querySelector('#description'),
+            genre           = document.querySelector('#genre'),
+            type            = document.querySelectorAll('input[name="type"]'),
+            bookForm        = document.querySelector('#book-form'),
+            typeValue;
 
+
+        showForm = function() {
+            bookForm.classList.toggle('d-none');
+        }
 
         bookForm.addEventListener('submit', function(e) {
           e.preventDefault();
           createBook();
-      })
+        })
 
-      createBook = function() {
-        type.forEach(function(el) {
-          if (el.checked) {
+        createBook = function() {
+            type.forEach(function(el) {
+            if (el.checked) {
             typeValue = el;
-          }
+            }
         })
 
         var books = [
-
-   {'id': 3, 'position': 3, 'title': 'Some title', 'description': 'some description', 'author': 'some author',  'date': 'date', 'genre': 'genre', 'type': 'private'}
+            {'id': 3, 'position': 3, 'title': 'Some title', 'description': 'some description', 'author': 'some author',  'date': 'date', 'genre': 'genre', 'type': 'private'}
         ];
 
-        // Get a reference to the comments list in the main DOM.
+
         var bookList = document.querySelector('.table-group');
 
-        // Loop through each of the comments and add them to the comments list.
+
         for (var i = 0; i < books.length; i++) {
-          var comment = books[i];
-          var tmpl = document.getElementById('comment-template').content.cloneNode(true);
-          tmpl.querySelector('.id').innerText = comment.id;
-          tmpl.querySelector('.position').innerText = comment.position;
-          tmpl.querySelector('.title').innerText = comment.title;
-          tmpl.querySelector('.description').innerText = comment.description;
-          tmpl.querySelector('.author').innerText = comment.author;
-          tmpl.querySelector('.date').innerText = comment.date;
-          tmpl.querySelector('.genre').innerText = comment.genre;
-          tmpl.querySelector('.type').innerText = comment.type;
-          bookList.appendChild(tmpl);
+            var comment = books[i];
+            var tmpl = document.getElementById('comment-template').content.cloneNode(true);
+            tmpl.querySelector('.id').innerText = comment.id;
+            tmpl.querySelector('.position').innerText = comment.position;
+            tmpl.querySelector('.title').innerText = comment.title;
+            tmpl.querySelector('.description').innerText = comment.description;
+            tmpl.querySelector('.author').innerText = comment.author;
+            tmpl.querySelector('.date').innerText = comment.date;
+            tmpl.querySelector('.genre').innerText = comment.genre;
+            tmpl.querySelector('.type').innerText = comment.type;
+            bookList.appendChild(tmpl);
         }
 
+      }
+
+        return {
+        showForm: function(data) {
+            if(data)
+            bookForm.classList.toggle('d-none');
+        }
       }
 
 
@@ -124,10 +188,19 @@ $(document).ready(function(){
 
 
 
-
-    var mediator = (function() {
-
+    var init = (function() {
+        mediator.subscribe( 'userLogin', showForm );
+        mediator.subscribe('greetings', function(arg) {
+            this.name = arg;
+            console.log(arg)
+        })
     })();
 
 
-});
+
+
+/*    mediator.subscribe('userLogin', function(count) {
+        alert(count)
+    })*/
+
+
