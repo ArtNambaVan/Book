e750929a80126e1d1796c9992e076b0ae7f5388f
Artem Klochkov <Arthorror@gmail.com>
2018-08-06 22:52:46 +0300

    Add mustache temp

--- dist/js/all.js
+++ dist/js/all.js
@@ -346,6 +346,13 @@ var bookTableController = (function() {
 var successAlert = (function() {
 
     var showAlert = function(obj) {
+
+
+        var tmpl = $('#user-name-template').html();
+        var html = Mustache.to_html(tmpl, obj);
+
+        $('#user-name').html(html);
+
         $('#successAlert').show('fade');
         setTimeout(function() {
             $('#successAlert').hide('fade');
--- src/js/modules/successAlert.js
+++ src/js/modules/successAlert.js
@@ -1,6 +1,13 @@
 var successAlert = (function() {
 
     var showAlert = function(obj) {
+
+
+        var tmpl = $('#user-name-template').html();
+        var html = Mustache.to_html(tmpl, obj);
+
+        $('#user-name').html(html);
+
         $('#successAlert').show('fade');
         setTimeout(function() {
             $('#successAlert').hide('fade');
